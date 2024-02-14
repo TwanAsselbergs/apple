09c241d4f899951d46b9695cfa6ef15eb5877122
TwanAsselbergs <twanasselbergs@outlook.com>
2024-02-14 11:05:22 +0100

    p

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// een array met de ids van de links
 var links = [
   "active-link-1",
   "active-link-2",
@@ -8,12 +9,16 @@ var links = [
   "active-link-7",
 ];
 
+// gaan door elke link in de array
 links.forEach(function (link) {
+  // voeg een klik event toe aan elke link
   document.getElementById(link).onclick = function () {
+    // als er op een link geklikt wordt, verwijdert de active-link klasse van alle links
     links.forEach(function (id) {
       document.getElementById(id).classList.remove("active-link");
     });
 
+    // voeg de active-link klasse toe aan de link waarop geklikt is
     this.classList.add("active-link");
   };
 });
